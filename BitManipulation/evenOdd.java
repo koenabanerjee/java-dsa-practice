5427ffdc772689e47a1264cc08af4989016fecec
Koena Banerjee <koenabanerjee966@gmail.com>
2025-08-27 22:33:55 +0530

    started with bit manipulation

---
+++ BitManipulation/evenOdd.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+public class evenOdd {
+
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if( (n & bitMask) == 0 ){
+            //even number
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+    
+    public static void main(String[] args) {
+        oddOrEven(3);
+        oddOrEven(11);
+        oddOrEven(14);
+    }
+}
